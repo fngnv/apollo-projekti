86764897c72a8c3024c0dc9826bf55e09c3af652
siljamat <siljama@hotmail.fi>
2023-02-13 11:19:27 +0200

    asiakaslkm ja simuloinnissa vietetty aika

---
+++ ApolloProjekti/src/simu/model/Suureet.java
@@ -0,0 +1,17 @@
+package simu.model;
+
+import java.util.ArrayList;
+/**
+public class Suureet {
+	public ArrayList<Double> palveluaika;
+
+	
+	public Suureet() {
+		palveluaika = new ArrayList<Double>();
+	}
+	
+	public  void setpalveluaika(double palvelunKesto) {
+	    palveluaika.add(palvelunKesto);
+	}
+}
+*/
